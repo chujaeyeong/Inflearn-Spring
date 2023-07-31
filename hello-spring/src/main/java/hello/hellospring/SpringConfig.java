30eaf5546e0bb97c1f9819d9e86281ac7d974357
jae yeong <chujy1224@gmail.com>
2023-08-01 00:09:57 +0900

    스프링입문 DAY05 - 섹션06 : 스프링 DB 접근 기술 - H2 데이터베이스 설치, 순수 JDBC fin.

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
     // DI에는 필드 주입, setter 주입, 생성자 주입 이렇게 3가지 방법이 있는데,
@@ -27,5 +37,10 @@ public class SpringConfig {
 
     // 실무에서는 주로 정형화된 컨트롤러, 서비스, 리포지토리 같은 코드는 컴포넌트 스캔을 사용한다 (오토와이어드, 서비스 어노테이션으로 자동 스캔해서 의존관계 주입하는방식)
     // 그리고 정형화 되지 않거나, 상황에 따라 구현 클래스를 변경해야 하면 설정을 통해 스프링 빈으로 등록한다.
-    
+
+    // 개방 폐쇄 원칙 (OCP, Open-closed Principle) : 확장에는 열려있고, 수정, 변경에는 닫혀있다.
+    // 스프링의 DI 를 사용하면 기존 코드를 전혀 손대지 않고, 설정만으로 구현 클래스를 변경할 수 있다.
+    // 회원을 등록하고 H2 DB 결과가 잘 입력됨!
+    // 데이터를 DB에 저장하므로 스프링 서버를 다시 실행해도 데이터가 안전하게 저장된다!
+
 }
