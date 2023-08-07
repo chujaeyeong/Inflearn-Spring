2d9b13450b25622d4f129f4e71ac11aa77f5adf0
jae yeong <chujy1224@gmail.com>
2023-08-07 21:02:17 +0900

    스프링 핵심 원리 DAY02 - 섹션02 : 스프링 핵심 원리 이해1 (예제 만들기) - 회원 도메인 실행과 테스트 fin.

---
+++ core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,24 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    // 애플리케이션 로직으로 이렇게 테스트 하는 것은 좋은 방법은 아님
+    // JUnit 테스트를 사용하자!
+    public static void main(String[] args) {
+
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP); // id 가 Long 타입이라 1L로 안 하면 (1로만 하면 컴파일오류남)
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+
+
+}
