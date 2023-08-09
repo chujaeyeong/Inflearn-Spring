7a95de7af9e4a6a07239cb882dfac67d226fbb47
jae yeong <chujy1224@gmail.com>
2023-08-10 02:16:13 +0900

    스프링 핵심 원리 DAY04 - 섹션03 : 스프링 핵심 원리 이해2 (객체 지향 원리 적용) - IoC, DI, 그리고 컨테이너 fin.

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -3,6 +3,7 @@ package hello.core;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,16 +12,21 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    /*
+     AppConfig 처럼 객체를 생성하고 관리하면서 의존관계를 연결해 주는 것을 IoC컨테이너 또는 DI컨테이너 라고 한다.
+     의존관계 주입에 초점을 맞추어 최근에는 주로 DI컨테이너 라고 한다. 또는 어샘블러, 오브젝트 팩토리 등으로 불리기도 한다.
+    */
+
     public MemberService memberService() {
-        return new MemberServiceImpl(MemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository MemberRepository() {
+    private MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     public DiscountPolicy discountPolicy() {
