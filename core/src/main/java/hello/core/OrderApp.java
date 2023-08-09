e88d4fbdb2c72049705bb83f4071f9520551f5f5
jae yeong <chujy1224@gmail.com>
2023-08-09 23:16:00 +0900

    스프링 핵심 원리 DAY03 - 섹션02 : 스프링 핵심 원리 이해1 (예제 만들기) - 주문과 할인 도메인 실행과 테스트 fin.

---
+++ core/src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,28 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    // psvm + 엔터 로 메인 함수 자동으로 만들 수 있음
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("order calculatePrice = " + order.calculatePrice());
+    }
+
+}
