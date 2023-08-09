d324df3ee53526cdf6d119cd2dd56906e787cde6
jae yeong <chujy1224@gmail.com>
2023-08-09 23:56:20 +0900

    스프링 핵심 원리 DAY03 - 섹션03 : 스프링 핵심 원리 이해2 (객체 지향 원리 적용) - 새로운 할인 정책 적용과 문제점 fin.

---
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,21 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10; // 10% 할인
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+        // 저게 10퍼 할인이 잘 되는지 궁금하면 바로 테스트 해보는게 좋다
+        // 컨트롤+쉬프트+T (원래는 커멘드+쉬프트+T인데... 쩝) 누르고 create new test 해주자
+
+    }
+}
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,8 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // 인터페이스에만 의존하도록 설계와 코드를 변경
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
