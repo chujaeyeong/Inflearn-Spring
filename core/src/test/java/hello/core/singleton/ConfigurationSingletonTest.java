d3f6df90649b691ad42f5186dc6b5dc310ce4469
jae yeong <chujy1224@gmail.com>
2023-08-18 02:45:00 +0900

    스프링 핵심 원리 DAY07 - 섹션05 : 싱글톤 컨테이너 - @Configration과 바이트코드 조작의 마법 fin.

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -29,4 +29,15 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // AppConfig도 스프링 빈으로 등록된다
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        // 출력 : bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$4e33cc3d
+    }
 }
