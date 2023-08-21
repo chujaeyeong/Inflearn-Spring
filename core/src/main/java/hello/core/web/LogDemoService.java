0b9efef36fb90d6564bf219c921a39635187d4e4
jae yeong <chujy1224@gmail.com>
2023-08-21 18:41:24 +0900

    스프링 핵심 원리 DAY09 - 섹션09 : 빈 스코프
    - 프로토타입 스코프 - 웝 스코프 ~ 스코프와 Provider fin.

---
+++ core/src/main/java/hello/core/web/LogDemoService.java
@@ -0,0 +1,22 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service id = " + id);
+    }
+}
