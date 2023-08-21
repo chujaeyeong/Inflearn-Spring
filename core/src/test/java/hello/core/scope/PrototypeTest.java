0cf690c2206266803fa0e33146554e70b1ce4855
jae yeong <chujy1224@gmail.com>
2023-08-21 15:48:08 +0900

    스프링 핵심 원리 DAY09 - 섹션09 : 빈 스코프
    - 빈 스코프란? ~ 프로토타입 스코프 fin.

---
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,45 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        ac.close();
+
+        // 프로토타입 스코프 빈은 클로즈가 안 됨... (종료메서드 호출이 안 됨)
+        // 프로토타입 빈은 생성, 의존관계, 초기화까지만 스프링에서 관리하고, 더는 관리하지 않기 때문에 수동으로 종료해줘야된다
+        prototypeBean1.destory();
+        prototypeBean2.destory();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destory() {
+            System.out.println("PrototypeBean.destory");
+        }
+    }
+}
