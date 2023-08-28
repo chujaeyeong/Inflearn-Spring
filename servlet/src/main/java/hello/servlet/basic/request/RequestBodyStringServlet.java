1a1404236609c5ff21d7a4ab542d53e111d10727
jae yeong <chujy1224@gmail.com>
2023-08-28 13:27:39 +0900

    스프링 MVC 1편 DAY01 - 섹션02 : 서블릿
    - HTTP 요청 데이터 (API 메시지 바디 - 단순 텍스트) fin.

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        // inputStream은 byte 코드를 반환하는데,
+        // byte 코드를 우리가 읽을 수 있는 문자(String)로 보려면 문자표 (Charset)을 지정해줘야 한다.
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("OK");
+
+    }
+}
