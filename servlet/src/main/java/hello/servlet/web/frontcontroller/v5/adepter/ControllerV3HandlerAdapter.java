fe71d8417fa8c4e7f25cca707b74da5a2112d4db
jae yeong <chujy1224@gmail.com>
2023-09-05 12:27:08 +0900

    스프링 MVC 1편 DAY04 - 섹션04 : MVC 프레임워크 만들기
    - 유연한 컨트롤러 (v5) fin.

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v5/adepter/ControllerV3HandlerAdapter.java
@@ -0,0 +1,43 @@
+package hello.servlet.web.frontcontroller.v5.adepter;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV3);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV3 controller = (ControllerV3) handler;
+
+        Map<String, String> paramMap = createParamMap(request);
+        ModelView mv = controller.process(paramMap);
+
+        return mv;
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        Enumeration<String> paramNames = request.getParameterNames();
+        while (paramNames.hasMoreElements()) {
+            String paramName = paramNames.nextElement();
+            paramMap.put(paramName, request.getParameter(paramName));
+        }
+        return paramMap;
+    }
+
+
+}
