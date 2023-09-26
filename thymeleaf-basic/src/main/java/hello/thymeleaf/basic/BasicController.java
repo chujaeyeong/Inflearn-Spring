b092b3aa933ae5d38d7fc015af33c669d486f0a3
jae yeong <chujy1224@gmail.com>
2023-09-26 12:39:35 +0900

    스프링 MVC 2편 DAY01 - 섹션01 : 타임리프 기본 기능
    - 변수 (SpringEL) fin.

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,14 +1,21 @@
 package hello.thymeleaf.basic;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/basic")
 public class BasicController {
 
+    // 텍스트 - text, utext
     @GetMapping("text-basic")
     public String textBasic(Model model) {
         model.addAttribute("data", "Hello Spring!");
@@ -19,6 +26,46 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-unescaped";
+    }
+
+
+    // 변수 - SpringEL
+    @GetMapping("/variable")
+    public String variable(Model model) {
+
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+
+    }
+
+
+
+    // 테스트를 위한 데이터 (User) 선언
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+
+
     }
 
 }
