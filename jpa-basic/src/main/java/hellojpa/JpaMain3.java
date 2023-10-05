4b52f298372b9ced02597ae5c53c1272bfa36374
jae yeong <chujy1224@gmail.com>
2023-10-05 12:07:34 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편
    DAY02) 섹션03 : 영속성 관리 - 내부 동작 방식
    - 준영속 상태 ~ 정리 fin.

--- jpa-basic/src/main/java/hellojpa/JpaMain3.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain3.java
@@ -19,7 +19,17 @@ public class JpaMain3 {
             // 영속
 
             Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
+            member.setName("AAAAA2"); // 변경내용 추적, 더티체킹
+
+            // 영속 상태의 엔티티가 영속성 컨텍스트에서 분리
+            // 영속성 컨텍스트가 제공하는 기능을 사용 못 함 >> 준영속상태, detach 상태라고 함
+            // detach(entity) : 특정 엔티티만 준영속 상태로 전환
+            // em.clear() : 영속성 컨텍스트를 완전히 초기화
+            // em.close() : 영속성 컨텍스트를 종료
+//            em.detach(member);
+            em.clear();
+
+            Member member2 = em.find(Member.class, 150L);
 
             System.out.println("=====================");
 
