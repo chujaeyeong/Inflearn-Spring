27097d2ad5e779e7929ea741601b4b42ac66834d
jae yeong <chujy1224@gmail.com>
2023-10-12 12:38:07 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술
    DAY06) 섹션04 : 검증1 - Validation
    - 오류코드와 메시지 처리 3 ~ 4 fin.

---
+++ SpringMVC2/validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,41 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+
+//        // 디테일한게 먼저 -> 덜 디테일한게 나중 우선순위
+//        for (String messageCode : messageCodes) {
+//            System.out.println("messageCode = " + messageCode);
+//        }
+
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+//        for (String messageCode : messageCodes) {
+//            System.out.println("messageCode = " + messageCode);
+//        }
+
+    }
+
+}
