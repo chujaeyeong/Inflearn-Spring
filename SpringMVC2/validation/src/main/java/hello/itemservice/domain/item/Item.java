372a0decccb8d1abcbf604a9dade5a700c0d4ee4
jae yeong <chujy1224@gmail.com>
2023-10-14 15:01:39 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술
    DAY08) 섹션05 : 검증2 - Bean Validation
    - Bean Validation 오브젝트 오류 fin.

--- SpringMVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ SpringMVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,15 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요.")
+//좀 억지로 쓴 느낌이라 그냥 컨트롤러에 복합 오브젝트 에러 검증 내용을 추가함
 public class Item {
 
     private Long id;
