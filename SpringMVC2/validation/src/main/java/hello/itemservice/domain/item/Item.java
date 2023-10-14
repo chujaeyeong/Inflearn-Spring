8152478a1e53ca78c0ce24ecbb7e9269d9265288
jae yeong <chujy1224@gmail.com>
2023-10-14 15:43:04 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술
    DAY08) 섹션05 : 검증2 - Bean Validation
    - Bean Validation 한계 ~ groups fin.

--- SpringMVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ SpringMVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -13,17 +13,18 @@ import javax.validation.constraints.NotNull;
 //좀 억지로 쓴 느낌이라 그냥 컨트롤러에 복합 오브젝트 에러 검증 내용을 추가함
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
     private Long id;
 
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
