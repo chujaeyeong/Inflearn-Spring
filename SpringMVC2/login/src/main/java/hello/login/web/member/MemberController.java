3b5e2ad930923e27e254e7a9f33e7a171f6d6a1a
jae yeong <chujy1224@gmail.com>
2023-10-16 12:24:10 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술
    DAY09) 섹션06 : 로그인 처리 1 - 쿠키, 세션
    - 회원가입 fin.

---
+++ SpringMVC2/login/src/main/java/hello/login/web/member/MemberController.java
@@ -0,0 +1,40 @@
+package hello.login.web.member;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute("member") Member member) {
+        // @ModelAttribute 라고만 써도 되는데 IDE 에서 인식을 잘 못해서 ("member") 를 적어줬다
+        return "members/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Valid @ModelAttribute Member member, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/addMemberForm";
+        }
+
+        memberRepository.save(member);
+        return "redirect:/";
+    }
+
+
+}
