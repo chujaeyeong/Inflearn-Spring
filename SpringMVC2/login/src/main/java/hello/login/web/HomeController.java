a6aba76250c48b30424f36a650e42e7cb50efe53
jae yeong <chujy1224@gmail.com>
2023-10-24 11:18:27 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술
    DAY11) 섹션06 : 로그인 처리 1 - 쿠키, 세션
    - 로그인 처리하기 (서블릿 HTTP 세션 2) fin.

--- SpringMVC2/login/src/main/java/hello/login/web/HomeController.java
+++ SpringMVC2/login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -63,7 +64,7 @@ public class HomeController {
 
 
     // 서블릿 Http 세션 1
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         HttpSession session = request.getSession(false);
@@ -72,6 +73,22 @@ public class HomeController {
         }
 
         Object loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+
+    }
+
+    // 서블릿 Http 세션 2
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
 
         // 세션에 회원 데이터가 없으면 home
         if (loginMember == null) {
