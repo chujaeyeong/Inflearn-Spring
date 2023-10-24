1f969620a6f0bd1fe018c736d2dda06f196cc54e
jae yeong <chujy1224@gmail.com>
2023-10-24 12:14:34 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술
    DAY11) 섹션07 : 서블릿 필터 - 요청 로그
    - 서블릿 필터 소개 ~ 요청 로그 fin.

---
+++ SpringMVC2/login/src/main/java/hello/login/web/filter/LogFilter.java
@@ -0,0 +1,44 @@
+package hello.login.web.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("log filter doFilter");
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        String uuid = UUID.randomUUID().toString();
+
+        try {
+            log.info("REQUEST [{}][{}]", uuid, requestURI);
+            // 다음 필터가 있으면 호출, 없으면 서블릿을 호출한다는 뜻임
+            // chain.doFilter 로직이 없으면 다음 단계로 진행되지 않기 때문에 빈 화면이 뜬다
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+
+}
