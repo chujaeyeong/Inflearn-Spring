aff964f174d3ae8fc93d7eeda986409fd0dfe14f
jae yeong <chujy1224@gmail.com>
2023-10-29 12:45:46 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술
    DAY13) 섹션09 : API 예외처리
    - 스프링 부트 기본 오류 처리 fin.
    - 스프링 부트에서 기본 제공하는 오류 매커니즘 (BasicErrorController) 을 사용하여 오류 API 생성

--- SpringMVC2/exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ SpringMVC2/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 // 스프링부트가 제공하는 기본 오류 메커니즘을 사용할거면 Component 어노테이션을 막아주자
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
