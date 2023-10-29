86850a4f88b61cbb8390cbf48163d1feefbff887
jae yeong <chujy1224@gmail.com>
2023-10-29 13:30:47 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술
    DAY13) 섹션09 : API 예외처리
    - HandlerExceptionResolver 시작 ~ 활용 fin.
    - HandlerExceptionResolver(ExceptionResolver) 를 사용하여 컨트롤러 밖으로 던져진 예외 해결 및 동작 방식 변경 학습
    - 사용자 정의 예외 (UserException) 를 추가하여 사용자 오류 발생 시 예외 처리

--- SpringMVC2/exception/src/main/java/hello/exception/WebConfig.java
+++ SpringMVC2/exception/src/main/java/hello/exception/WebConfig.java
@@ -2,14 +2,17 @@ package hello.exception;
 
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
+import hello.exception.resolver.MyHandlerExceptionResolver;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -23,7 +26,13 @@ public class WebConfig implements WebMvcConfigurer {
                 );
     }
 
-//    @Bean
+    // 기본 설정을 유지하면서 추가
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+    }
+
+    //    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
