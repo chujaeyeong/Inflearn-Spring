acff6d82b779fb1fa2c988edf50a480b796cce20
jae yeong <chujy1224@gmail.com>
2023-10-30 11:58:30 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술
    DAY14) 섹션09 : API 예외처리
    - 스프링이 제공하는 ExceptionResolver 1 ~ 2 fin.
    - ExceptionResolver 중 2가지 학습
    - 1) ResponseStatusExceptionResolver : HTTP 응답 코드 변경할 때 사용
    - 2) DefaultHandlerExceptionResolver : 스프링 내부 예외의 상태코드를 변경하는 기능을 제공

--- SpringMVC2/exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ SpringMVC2/exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,12 +1,16 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -25,6 +29,25 @@ public class ApiExceptionController {
             throw new UserException("사용자 오류");
         }
         return new MemberDto(id, "hello " + id);
+    }
+
+
+    // 스프링이 제공하는 ExceptionResolver1 - ResponseStatusExceptionResolver(@ResponseStatus) : HTTP 응답 코드 변경
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    // 스프링이 제공하는 ExceptionResolver1 - @ResponseStatus 메시지 추가 가능 (message.properties에 메시지 추가)
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
+    // 스프링이 제공하는 ExceptionResolver2 - DefaultHandlerExceptionResolver : 스프링 내부 예외 처리
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
     }
 
 
