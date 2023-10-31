3307cfbcd0a1c811face336cea45034d1b8aa356
jae yeong <chujy1224@gmail.com>
2023-10-31 15:05:41 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술
    DAY15) 섹션10 : 스프링 타입 컨버터
    - 포맷터 Formatter ~ 스프링이 제공하는 기본 포맷터 fin.
    - 컨버터는 범용 타입 변환을 제공하고, 포맷터는 특정한 포맷에 맞춰서 문자를 출력하거나 그 반대의 역할을 하는 것에 특화된 기능
    - 컨버터와 포맷터가 같이 있으면 컨버터가 우선순위로 적용됨
    - 숫자 관련 형식 지정 포맷터 (@NumberFormat) 와 날짜 관련 형식 지정 포맷터 (@DateTimeFormat) 는 알아두면 유용함

--- SpringMVC2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ SpringMVC2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistrar;
 import org.springframework.format.FormatterRegistry;
@@ -15,9 +16,14 @@ public class WebConfig implements WebMvcConfigurer {
     // 웹 애플리케이션에 Converter 등록
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        // 포맷터를 사용하기 위해서 기존 등록했던 컨버터를 주석처리해준다
+        // 컨버터가 포맷터보다 우선순위
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        // 포맷터 추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
