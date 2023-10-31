77295be2c7b9b5663bce13d72709fafaf62f09d7
jae yeong <chujy1224@gmail.com>
2023-10-31 14:05:04 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술
    DAY15) 섹션10 : 스프링 타입 컨버터
    - 스프링에 Converter 적용하기 fin.
    - 만들어놨던 컨버터를 WebConfig에 등록 후 Conteroller에 추가해서 실행

---
+++ SpringMVC2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -0,0 +1,23 @@
+package hello.typeconverter;
+
+import hello.typeconverter.converter.IntegerToStringConverter;
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistrar;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // 웹 애플리케이션에 Converter 등록
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+    }
+}
