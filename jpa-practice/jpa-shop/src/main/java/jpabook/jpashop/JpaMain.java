8754f65213519b23c2cb3da36e77e1b9cd6d0b9a
jae yeong <chujy1224@gmail.com>
2023-11-23 21:32:54 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편
    DAY04) 섹션04 : 엔티티 매핑
    - 실전 예제1 요구사항 분석과 기본 매핑 fin.
    - 요구사항에 맞춰서 테이블 설계 및 기본 매핑 완료
    - 엔티티 설계와 매핑 진행 -> 현재는 객체 설계를 테이블 설계에 맞춰서 진행함. 다음 단계에서 연관관계 매핑으로 디벨롭 예정

---
+++ jpa-practice/jpa-shop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            tx.commit();
+
+        } catch (Exception e) {
+
+            tx.rollback();
+
+        } finally {
+
+            em.close();
+        }
+
+        // DB를 다 쓰면 닫아줘야된다
+        emf.close();
+    }
+}
