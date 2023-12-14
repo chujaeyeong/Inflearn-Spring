10cdafde1480360eba128de908b40bd34d1b98fe
jae yeong <chujy1224@gmail.com>
2023-12-14 17:33:26 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편
    DAY09) 섹션10 : 객체지향 쿼리 언어1 - 기본 문법
    - 프로젝션(SELECT) fin. : SELECT 절에 조회할 대상을 지정하는 프로젝션 방법 학습

--- jpa-practice/jpql-basic/src/main/java/jpql/JpaMain.java
+++ jpa-practice/jpql-basic/src/main/java/jpql/JpaMain.java
@@ -17,12 +17,32 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
+            em.flush();
+            em.clear();
 
-            System.out.println("result = " + result.getUsername());
+//            List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList(); // 엔티티 프로젝션
+//            List<Team> result = em.createQuery("select m.team from Member m", Team.class).getResultList(); // 엔티티 프로젝션 (이렇게 하면 돌아가긴 하는데 헷갈리니까 조인 문법으로 써야된다)
+//            em.createQuery("select o.address from Order o", Address.class).getResultList(); // 임베디드 타입 프로젝션 (소속을 꼭 맞춰서 적어야된다)
+//            em.createQuery("select distinct m.username, m.age from Member m").getResultList(); // 스칼라 타입 프로젝션, DISTINCT로 중복 제거
 
+            // 프로젝션 - 여러값 조회 : new 명령어로 조회
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO = " + memberDTO.getUsername());
+            System.out.println("memberDTO = " + memberDTO.getAge());
+
+
+//            Member findMember = result.get(0);
+//            findMember.setAge(20);
+
+
+//            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+//                    .setParameter("username", "member1")
+//                    .getSingleResult();
+//
+//            System.out.println("result = " + result.getUsername());
 
 //            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class); // 반환 타입이 명확할 때 사용
 //            Query query3 = em.createQuery("select m.username, m.age from Member m"); // 반환 타입이 명확하지 않을 때 사용
