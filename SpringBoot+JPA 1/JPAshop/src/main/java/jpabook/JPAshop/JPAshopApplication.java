1c3f45ee1031c97af62a2458f1fe89a1bed3227f
jae yeong <chujy1224@gmail.com>
2023-12-21 15:19:34 +0900

    스프링 부트와 JPA 활용 1 - 웹 애플리케이션 개발
    DAY01) 섹션1 : 프로젝트 환경설정
    - 프로젝트 생성 fin.

---
+++ SpringBoot+JPA 1/JPAshop/src/main/java/jpabook/JPAshop/JPAshopApplication.java
@@ -0,0 +1,13 @@
+package jpabook.JPAshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPAshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JPAshopApplication.class, args);
+	}
+
+}
---
+++ SpringBoot+JPA 1/JPAshop/src/main/java/jpabook/JPAshop/hello.java
@@ -0,0 +1,9 @@
+package jpabook.JPAshop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class hello {
+    private String data;
+}
