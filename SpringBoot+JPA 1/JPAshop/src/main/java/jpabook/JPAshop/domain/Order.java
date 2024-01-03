9cceeb6523eb3b254c84188b95805d07ac1943e1
jae yeong <chujy1224@gmail.com>
2024-01-04 01:39:02 +0900

    스프링 부트와 JPA 활용 1 - 웹 애플리케이션 개발
    DAY05) 섹션6 : 주문 도메인 개발
    - 주문, 주문상품 엔티티 개발 fin. : 주문 생성, 주문 취소, 전체 주문 가격 조회 기능 담당 엔티티 개발 / 주문 상품쪽도 동일하게 생성, 취소, 가격 조회 기능 담당 엔티티 개발
    - 주문 리포지토리 개발 fin. : 검색 메서드 제외한 주문 엔티티 저장을 담당하는 리포지토리 개발

--- SpringBoot+JPA 1/JPAshop/src/main/java/jpabook/JPAshop/domain/Order.java
+++ SpringBoot+JPA 1/JPAshop/src/main/java/jpabook/JPAshop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.JPAshop.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -8,6 +10,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "orders")
+@Getter @Setter
 public class Order {
 
     @Id
@@ -46,4 +49,41 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    // == 생성 메서드 == //
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // == 비즈니스 로직 == //
+    /** 주문 취소 */
+    public void cancle() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancle();
+        }
+    }
+
+    // == 조회 로직 == //
+    /** 전체 주문 가격 조회 */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 }
---
+++ SpringBoot+JPA 1/JPAshop/src/main/java/jpabook/JPAshop/repository/OrderRepository.java
@@ -0,0 +1,28 @@
+package jpabook.JPAshop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.JPAshop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//    }
+
+}
