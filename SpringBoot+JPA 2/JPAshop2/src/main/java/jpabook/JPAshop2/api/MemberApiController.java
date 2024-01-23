73d70a56a00216c009a61d6390016e3e11edf6dc
jae yeong <chujy1224@gmail.com>
2024-01-24 00:04:53 +0900

    스프링 부트와 JPA 활용 2 - API 개발과 성능 최적화
    DAY02) 섹션1 : API 개발 기본
    - 회원 등록 API fin. : 엔티티를 Request Body에 직접 매핑 방법 (v1) -> 엔티티 대신 DTO를 RequestBody 에 매핑 방법으로 디벨롭
    - 회원 수정 API fin. : 변경 감지를 사용해서 데이터 수정 (여기서는 PUT을 사용했는데, 부분 업데이트를 위해서는 PATCH나 POST를 사용하는 것이 바람직하다)
    - 회원 조회 API fin. : 응답 값으로 엔티티를 직접 외부에 노출시키는 방법 (v1) -> 응답 값으로 엔티티가 아닌 별도의 DTO로 한 번 변환 후에 반환하는 방법으로 디벨롭

---
+++ SpringBoot+JPA 2/JPAshop2/src/main/java/jpabook/JPAshop2/api/MemberApiController.java
@@ -0,0 +1,108 @@
+package jpabook.JPAshop2.api;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jpabook.JPAshop2.domain.Member;
+import jpabook.JPAshop2.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    // 이렇게 엔티티를 직접 반환해서는 안된다
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    // 엔티티를 DTO로 한 번 변환 후 반환한다
+    // 장점 : 엔티티가 변해도 API 스펙이 변경되지 않는다
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        // 엔티티 -> DTO 변환
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+
+
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    public class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+}
