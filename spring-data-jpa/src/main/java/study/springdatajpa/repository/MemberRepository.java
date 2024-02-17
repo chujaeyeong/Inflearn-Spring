0232ac030c4cabc6a924c5917fcc6b4311bd6772
jae yeong <chujy1224@gmail.com>
2024-02-18 00:41:08 +0900

    스프링 데이터 JPA
    DAY03) 섹션4 : 쿼리 메소드 기능
    - 메소드 이름으로 쿼리 생성 : 쿼리 메소드 필터 조건에 맞춰 원하는 메소드를 생성해서 JPQL 실행, 테스트 진행

--- spring-data-jpa/src/main/java/study/springdatajpa/repository/MemberRepository.java
+++ spring-data-jpa/src/main/java/study/springdatajpa/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package study.springdatajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.springdatajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
 }
--- spring-data-jpa/src/test/java/study/springdatajpa/repository/MemberRepositoryTest.java
+++ spring-data-jpa/src/test/java/study/springdatajpa/repository/MemberRepositoryTest.java
@@ -63,4 +63,19 @@ public class MemberRepositoryTest {
 
     }
 
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+
+    }
+
 }
