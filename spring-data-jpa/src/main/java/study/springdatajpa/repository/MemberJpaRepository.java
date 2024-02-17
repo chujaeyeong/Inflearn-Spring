91298cf38acaf7cb35da6028308a3e643f78be12
jae yeong <chujy1224@gmail.com>
2024-02-17 22:56:09 +0900

    스프링 데이터 JPA
    DAY02) 섹션3 : 공통 인터페이스 기능
    - 순수 JPA 기반 리포지토리 만들기 : CRUD 담당하는 MemberJpaRepository, TeamRepository 코드를 순수 JPA로 작성, 테스트 진행

--- spring-data-jpa/src/main/java/study/springdatajpa/repository/MemberJpaRepository.java
+++ spring-data-jpa/src/main/java/study/springdatajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.springdatajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -16,8 +19,25 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select  count(m) from Member m", Long.class).getSingleResult();
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
     }
 
 }
