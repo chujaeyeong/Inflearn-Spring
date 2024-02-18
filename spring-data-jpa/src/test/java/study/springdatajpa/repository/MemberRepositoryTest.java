cf8da4130435e036e3577a2baa0dbfe210f8b17e
jae yeong <chujy1224@gmail.com>
2024-02-18 21:01:38 +0900

    스프링 데이터 JPA
    DAY03) 섹션4 : 쿼리 메소드 기능
    - JPA NamedQuery : NamedQuery 어노테이션으로 Named 쿼리를 정의하고, 호출

--- spring-data-jpa/src/test/java/study/springdatajpa/repository/MemberRepositoryTest.java
+++ spring-data-jpa/src/test/java/study/springdatajpa/repository/MemberRepositoryTest.java
@@ -78,4 +78,17 @@ public class MemberRepositoryTest {
 
     }
 
+    @Test
+    public void testNamedQuery() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsername("AAA");
+        Member findMember = result.get(0);
+        assertThat(findMember).isEqualTo(m1);
+
+    }
+
 }
